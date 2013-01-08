e91e64e7b5b2cfed0837887038059f85bbaa5a73
Qijiang Fan <fqj1994@gmail.com>
2013-01-08 11:37:59 +0800

    在mysql上缓存simsimi的回复。

--- simsimi.py
+++ simsimi.py
@@ -28,6 +28,14 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import requests
 import cookielib
+import MySQLdb
+import random
+import hashlib
+import time
+from settings import MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DBNAME
+
+
+rd = random.SystemRandom()
 
 
 class SimSimi:
@@ -45,13 +53,67 @@ class SimSimi:
 
         self.chat_url = 'http://www.simsimi.com/func/req?lc=ch&msg=%s'
         self.teach_url = 'http://www.simsimi.com/func/teach'
+        self.local_cache = {}
+
+    def _int32_hashcode(self, message=''):
+        return int(int(hashlib.md5(message).hexdigest(), 16) % (2 ** 30))
+
+    def read_cache(self, message=''):
+        if message:
+            if message in self.local_cache and self.local_cache[message]:
+                return rd.choice(self.local_cache[message])
+            conn = None
+            res = None
+            try:
+                conn = MySQLdb.connect(host=MYSQL_HOST, user=MYSQL_USER, passwd=MYSQL_PASS,
+                        db=MYSQL_DBNAME, charset="utf8", use_unicode=False)
+                cursor = conn.cursor()
+                sql = "SELECT * FROM simsimi_answers WHERE hashcode = %s AND question = %s AND creationtime >= %s"
+                cursor.execute(sql, (self._int32_hashcode(message), message, int(time.time() - 3600 * 24)))
+                l = cursor.fetchall()
+                self.local_cache[message] = list(set([k[2] for k in l]))
+                if l:
+                    res = rd.choice(list(set(l)))[2]
+            finally:
+                if conn:
+                    conn.close()
+            return res
+        else:
+            return None
+
+    def write_to_cache(self, message='', answer=''):
+        if message and answer:
+            conn = None
+            try:
+                conn = MySQLdb.connect(host=MYSQL_HOST, user=MYSQL_USER, passwd=MYSQL_PASS,
+                        db=MYSQL_DBNAME, charset="utf8", use_unicode=False)
+                cursor = conn.cursor()
+
+                #理论上来说需要事务，但实际环境中，不需要严格的事务，重复了就重复了呗。
+                sql_search = "SELECT * FROM simsimi_answers WHERE hashcode = %s AND question = %s AND answer = %s"
+                sql_insert = "INSERT INTO simsimi_answers (hashcode, question, answer, creationtime) VALUES(%s, %s, %s, %s)"
+                sql_update = "UPDATE simsimi_answers SET creationtime = %s WHERE hashcode = %s AND question = %s AND answer = %s"
+                cursor.execute(sql_search, (self._int32_hashcode(message), message, answer))
+                if cursor.fetchone():
+                    cursor.execute(sql_update, (int(time.time()), self._int32_hashcode(message), message, answer))
+                else:
+                    cursor.execute(sql_insert, (self._int32_hashcode(message), message, answer, int(time.time())))
+            except Exception as e:
+                print e
+            finally:
+                if conn:
+                    conn.close()
 
     def chat(self, message=''):
         if message:
+            if rd.choice([True, True, False]) and self.read_cache(message):
+                return self.read_cache(message).decode('UTF-8')
             r = requests.get(self.chat_url % message, cookies=self.chat_cookies, headers=self.headers)
             self.chat_cookies = r.cookies
             try:
-                return r.json()['response']
+                answer = r.json()['response']
+                self.write_to_cache(message, answer)
+                return answer
             except:
                 return u'呵呵'
         else:
